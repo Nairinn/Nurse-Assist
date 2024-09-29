6fdf7df313ef0d8369b93dd80eea0b310641159d
Nairinn <naingrais@gmail.com>
2024-09-29 00:57:22 -0700

    HIIIIIIIIII MONKAAAAAYYY

--- ImGettingShocked/src/App.js
+++ ImGettingShocked/src/App.js
@@ -1,19 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import AddPatientForm from './AddPatientForm';
 import PatientDataTable from './PatientDataTable';
+import Login from './Login'; // Import your Login component
 
 const App = () => {
   return (
-    <div>
-      <div className='bg-customRed border-3 border-gray'>
-        <h1 className="flex items-center justify-center text-white font-bold text-2xl">
-             NurseAssist
-        </h1>
+    <Router>
+      <div>
+        <div className='bg-customRed border-3 border-gray'>
+          <h1 className="flex items-center justify-center text-white font-bold text-2xl">
+            NurseAssist
+          </h1>
+        </div>
+        
+        {/* Navigation Links */}
+        <div className="flex justify-center mt-4">
+          <Link to="/login" className="text-blue-500 hover:underline">
+            Go to Login
+          </Link>
+        </div>
+
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddPatientForm />
+              <PatientDataTable />
+            </>
+          } />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
-          <AddPatientForm />
-          <PatientDataTable />
-    </div>
+    </Router>
   );
 };
+
 export default App;
