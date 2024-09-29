d132fbab26136eef1890d955d7cc178e99de143c
JBarroqu <johnnessambarroquillo@gmail.com>
2024-09-28 23:52:49 -0700

    add header, greeting, and add patient button

---
+++ ImGettingShocked/src/AddButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPatientButton = () => {
+  return (
+    <button className="bg-green-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-green-600">
+      Add Patient
+    </button>
+  );
+};
+
+export default AddPatientButton;
--- ImGettingShocked/src/App.js
+++ ImGettingShocked/src/App.js
@@ -3,15 +3,29 @@ import CurrentData from './CurrentData'
 import './App.css';
 import LastMeasured from './LastMeasured';
 import SleepForm from './SleepForm';
+import Card from './Card';
+import AddButton from './AddButton'
+import Greeting from './Greeting';
 
 const App = () => {
   return (
     <div>
-      <div className='bg-customRed border-3 border-gray'>
-        <h1 className="flex items-center justify-center text-white font-bold text-2xl">
-             NurseAssist
+      <div className='bg-customRed border-full border-gray-500'>
+        <h1 className="flex items-center justify-center text-white font-bold text-2xl py-3 ">
+             NurseAssist 
+             <img src='/doctor.png' alt='health symbol' className='w-7 h-7 object-cover mx-2'></img>
         </h1>
+        
       </div>
+      <div className="border-2xlS border border-solid border-red-800 w-full"></div>
+
+      <Greeting isLoggedIn={true} name="Norton"></Greeting>
+      <AddButton></AddButton>
+      
+      
+
+      <Card title="RPM"/>
+
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
       <div className="flex flex-col gap-6">
