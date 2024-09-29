85b85584b2a3b95b9f3d94da78377b4eb732bf5a
JBarroqu <johnnessambarroquillo@gmail.com>
2024-09-29 01:43:50 -0700

    add green

--- ImGettingShocked/src/Greeting.jsx
+++ ImGettingShocked/src/Greeting.jsx
@@ -3,13 +3,13 @@ function Greeting(props){
     <div className="bg-gray-200 p-4 shadow-md">
         {props.isLoggedIn ? (
         <h1 className="text-6xl text-white">
-          <span className="bg-customRed bg-opacity-25">
+          <span className="bg-customGreen bg-opacity-25">
             Good Day, Dr. {props.name}
           </span>
         </h1>
       ) : (
         <h1 className="text-m text-white">
-          <span className="bg-customRed bg-opacity-25">
+          <span className="bg-customGreen bg-opacity-25">
             Please log in...
           </span>
         </h1>
